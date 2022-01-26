85dc716258fd4f4631d27a8f4bbe2f35b660f237
marcelogaldino <marcelinhomgo@gmail.com>
2022-01-26 01:04:51 -0300

    feat: added new task, remove task, mark as done an

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,28 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldState => [...oldState, data])
+    console.log(data)
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }))
+    const taskFounded = updatedTasks.find(task => task.id === id)
+    if (!taskFounded) return
+
+    taskFounded.done = !taskFounded.done
+
+    setTasks([...updatedTasks])
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(task => task.id !== id))
   }
 
   return (
@@ -26,10 +39,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
